e5e53d59814c00de0e58ddfc92cd0d6e5ade2130
Sree1Lekha <57091379+Sree1Lekha@users.noreply.github.com>
2024-01-17 18:08:16 +0530

    Create subs.py

---
+++ src/subs.py
@@ -0,0 +1,12 @@
+# app.py
+# This is a test commit
+# sub
+def sub(a, b):
+    return a - b
+
+def test_sub():
+    assert sub(3, 2) == 1
+    assert sub(7, 1) == 6
+   # assert sub(5, 10) == 15
+
+
